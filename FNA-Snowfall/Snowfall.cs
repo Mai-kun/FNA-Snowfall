34326a947d370ffa372af9317f9a88486c659c2c
Mai-kun <amaz1ngcreature10@mail.ru>
2024-10-22 19:34:52 +0300

    Проект готоа

--- FNA-Snowfall/Snowfall.cs
+++ FNA-Snowfall/Snowfall.cs
@@ -14,22 +14,38 @@ namespace FNA_Snowfall
         private Texture2D snowflakeTexture;
         private List<Snowflake> snowflakes;
 
-        private const int WindowHeight = 1920;
-        private const int WindowWidth = 1080;
+        private readonly int windowHeight;
+        private readonly int windowWidth;
         private readonly Random random = new Random();
+        private MouseState previousMouseState;
 
         public Snowfall()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
+            windowWidth = graphics.PreferredBackBufferWidth;
+            windowHeight = graphics.PreferredBackBufferHeight;
+
+            IsMouseVisible = false;
             graphics.IsFullScreen = true;
-            graphics.ApplyChanges();
         }
 
         protected override void Initialize()
         {
             base.Initialize();
+
+            snowflakes = new List<Snowflake>();
+            for (var i = 0; i < 400; i++)
+            {
+                var size = (float)random.Next(3, 7) / 100;
+                var speed = (float)random.NextDouble() * 50f + 20f;
+                var startPosition = new Vector2(random.Next(0, windowWidth), random.Next(0, windowHeight));
+
+                snowflakes.Add(new Snowflake(startPosition, speed, size));
+            }
+
+            previousMouseState = Mouse.GetState();
         }
 
         protected override void LoadContent()
@@ -37,35 +53,33 @@ namespace FNA_Snowfall
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             snowflakeTexture = Content.Load<Texture2D>("snowflake");
-            snowflakes = new List<Snowflake>();
-
-            for (var i = 0; i < 200; i++)
-            {
-                var size = (float)random.Next(3, 7) / 100;
-                var speed = (float)random.NextDouble() * 50f + 20f;
-                var startPosition = new Vector2(random.Next(0, WindowWidth), random.Next(0, WindowHeight));
-
-                snowflakes.Add(new Snowflake(snowflakeTexture, startPosition, speed, size));
-            }
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            var currentMouseState = Mouse.GetState();
+            if (currentMouseState.X != previousMouseState.X ||
+                currentMouseState.Y != previousMouseState.Y ||
+                Keyboard.GetState().GetPressedKeys().Length > 0)
             {
                 Exit();
             }
 
+            UpdateSnowFlakePosition(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        private void UpdateSnowFlakePosition(GameTime gameTime)
+        {
             foreach (var snowflake in snowflakes)
             {
                 snowflake.Fall(gameTime);
-                if (snowflake.position.Y > WindowHeight)
+                if (snowflake.Position.Y > windowHeight)
                 {
-                    snowflake.position = new Vector2(random.Next(0, WindowWidth), -50);
+                    snowflake.Position = new Vector2(random.Next(0, windowWidth), -50);
                 }
             }
-
-            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -76,7 +90,7 @@ namespace FNA_Snowfall
 
             foreach (var snowflake in snowflakes)
             {
-                snowflake.Draw(spriteBatch);
+                spriteBatch.Draw(snowflakeTexture, snowflake.Position, null, Color.White, 0f, Vector2.Zero, snowflake.Size, SpriteEffects.None, 0f);
             }
 
             spriteBatch.End();
--- FNA-Snowfall/Snowflake.cs
+++ FNA-Snowfall/Snowflake.cs
@@ -1,38 +1,34 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace FNA_Snowfall
 {
     internal class Snowflake
     {
-        public Vector2 position;
+        /// <summary>
+        /// Расположение
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// Размер
+        /// </summary>
+        public float Size { get; }
 
         private readonly float speed;
-        private readonly float size;
-        private readonly Texture2D texture;
 
-        public Snowflake(Texture2D texture, Vector2 startPosition, float speed, float size)
+        public Snowflake(Vector2 startPosition, float speed, float size)
         {
-            this.texture = texture;
-            position = startPosition;
+            Position = startPosition;
             this.speed = speed;
-            this.size = size;
+            Size = size;
         }
 
         /// <summary>
-        /// Перемещает <see cref="Snowflake"/> вниз на заданнок число <see cref="Snowflake.speed"/>
+        /// Перемещает <see cref="Snowflake"/> вниз на заданное число <see cref="Snowflake.speed"/>
         /// </summary>
         public void Fall(GameTime gameTime)
         {
-            position.Y += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-        }
-
-        /// <summary>
-        /// Рисует <see cref="Snowflake"/>
-        /// </summary>
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(texture, position, null, Color.White, 0f, Vector2.Zero, size, SpriteEffects.None, 0f);
+            Position = new Vector2(Position.X, Position.Y + speed * (float)gameTime.ElapsedGameTime.TotalSeconds);
         }
     }
 }
